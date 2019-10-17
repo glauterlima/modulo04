668193922dd7bc03c3e8fa6ddaed9f67a3392ed4
Glauter Santos <glauterlima@gmail.com>
2019-10-17 20:40:52 -0300

    Continuando API do GoBarber: Configurando templates de e-mail concluido

---
+++ bootCampRocketSeat/gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '9e66c2e934dfb3',
+        pass: '5c03647424d215',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
