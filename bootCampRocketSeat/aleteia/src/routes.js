6d428276dc1a35510b828869673ea5d029d4763c
Glauter Santos <glauterlima@gmail.com>
2019-12-13 17:49:01 -0300

    Cadastro de usuários - concluido

--- bootCampRocketSeat/aleteia/src/routes.js
+++ bootCampRocketSeat/aleteia/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Glauter Santos',
-    email: 'glauter.santos@trf1.jus.br',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
