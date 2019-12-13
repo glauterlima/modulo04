f3b8939246cdb8304084c9330b71d156939a8cda
Glauter Santos <glauterlima@gmail.com>
2019-12-13 17:32:06 -0300

    Criando loader de models - concluido

--- bootCampRocketSeat/aleteia/src/routes.js
+++ bootCampRocketSeat/aleteia/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Glauter Santos',
+    email: 'glauter.santos@trf1.jus.br',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
